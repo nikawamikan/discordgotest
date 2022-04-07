7c2ea85f032c42af257a1ad19028ad5f1c77d4d3
nikawamikan <wkwktktkkrkr@outlook.jp>
2022-04-07 20:21:53 +0900

    コメント追加とか

--- commands/base.go
+++ commands/base.go
@@ -8,22 +8,26 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// structの方が微妙に使いやすいので定義（java民の感想）
 type commandBase struct {
 	commands        []*discordgo.ApplicationCommand
 	commandHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
 }
 
-var com commandBase
-
-func init() {
-	com.commands = make([]*discordgo.ApplicationCommand, 0, 100)
-	com.commandHandlers = make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate), 100)
-}
-
+// コマンドのフロント側の名称をそのまま関数のMapに使用してsetします
 func (c *commandBase) addCommand(com *discordgo.ApplicationCommand, fn func(s *discordgo.Session, i *discordgo.InteractionCreate)) *commandBase {
 	c.commands = append(c.commands, com)
 	c.commandHandlers[com.Name] = fn
 	return c
+}
+
+// 空のオブジェクト定義
+var com commandBase
+
+// 空の配列長無駄に100定義してるけど全然たぶん必要ないので考えとく（たぶんそこまでパフォーマンスに影響でないので0でも良いとは思う）
+func init() {
+	com.commands = make([]*discordgo.ApplicationCommand, 0, 100)
+	com.commandHandlers = make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate), 100)
 }
 
 // なんか頭大文字だと外部参照可になるらしい
