9abf5a8d95276bcb0a2f4ca35050196a0cc0826f
nikawamikan <wkwktktkkrkr@outlook.jp>
2022-04-07 20:02:49 +0900

    フレームワーク改善addCommandメソッドの定義

--- commands/base.go
+++ commands/base.go
@@ -8,20 +8,30 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var (
-	// slashcommandのフロント部分の定義
-	commands = make([]*discordgo.ApplicationCommand, 0, 20)
+type commandBase struct {
+	commands        []*discordgo.ApplicationCommand
+	commandHandlers map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate)
+}
 
-	// slashcommandの内容部分の定義
-	commandHandlers = make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate), 20)
-)
+var com commandBase
+
+func init() {
+	com.commands = make([]*discordgo.ApplicationCommand, 0, 100)
+	com.commandHandlers = make(map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate), 100)
+}
+
+func (c *commandBase) addCommand(com *discordgo.ApplicationCommand, fn func(s *discordgo.Session, i *discordgo.InteractionCreate)) *commandBase {
+	c.commands = append(c.commands, com)
+	c.commandHandlers[com.Name] = fn
+	return c
+}
 
 // なんか頭大文字だと外部参照可になるらしい
 func GetCommands() []*discordgo.ApplicationCommand {
-	return commands
+	return com.commands
 }
 
 // なんか頭大文字じゃないとだめらしい
 func GetCommandHandlers() map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	return commandHandlers
+	return com.commandHandlers
 }
--- commands/test.go
+++ commands/test.go
@@ -5,90 +5,36 @@ package commands
 */
 
 import (
-	"fmt"
-
 	"github.com/bwmarrin/discordgo"
-)
-
-var (
-	test  = "test"
-	basic = "basic"
 )
 
 func init() {
 	// コマンドの追加
-	commands = append(commands, []*discordgo.ApplicationCommand{
-		{
-			Name:        test,            // コマンド名
-			Description: "Basic command", // コマンドーの説明
-			Options: []*discordgo.ApplicationCommandOption{ // Optionを追加する
-				{
-					Type:        discordgo.ApplicationCommandOptionString, // オプションの種類
-					Name:        "name",                                   // 名前
-					Description: "String option",                          // オプションの説明
-					Required:    true,                                     // 必須
-				},
-			},
+	com.addCommand(
+		&discordgo.ApplicationCommand{
+			Name:        "command",
+			Description: "説明",
 		},
-		{
-			Name:        basic,           // コマンド名
-			Description: "Basic command", // コマンドーの説明
-			Options: []*discordgo.ApplicationCommandOption{ // Optionを追加する
-				{
-					Type:        discordgo.ApplicationCommandOptionString, // オプションの種類
-					Name:        "name",                                   // 名前
-					Description: "String option",                          // オプションの説明
-					Required:    true,                                     // 必須
+		func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "test",
 				},
-			},
+			})
 		},
-	}...)
-
-	// 内容の追加
-	commandHandlers[test] = func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		options := i.ApplicationCommandData().Options
-
-		optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
-		for _, opt := range options {
-			optionMap[opt.Name] = opt
-		}
-
-		var name string
-		msgformat := "test options "
-
-		if option, ok := optionMap["name"]; ok {
-			name = option.StringValue()
-			msgformat += "\nname: %s"
-		}
-
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf(msgformat, name),
-			},
-		})
-	}
-	commandHandlers[basic] = func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		options := i.ApplicationCommandData().Options
-
-		optionMap := make(map[string]*discordgo.ApplicationCommandInteractionDataOption, len(options))
-		for _, opt := range options {
-			optionMap[opt.Name] = opt
-		}
-
-		var name string
-		msgformat := "test options "
-
-		if option, ok := optionMap["name"]; ok {
-			name = option.StringValue()
-			msgformat += "\nname: %s"
-		}
-
-		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-			Type: discordgo.InteractionResponseChannelMessageWithSource,
-			Data: &discordgo.InteractionResponseData{
-				Content: fmt.Sprintf(msgformat, name),
-			},
-		})
-	}
+	).addCommand(
+		&discordgo.ApplicationCommand{
+			Name:        "command2",
+			Description: "説明",
+		},
+		func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: "test",
+				},
+			})
+		},
+	)
 }
